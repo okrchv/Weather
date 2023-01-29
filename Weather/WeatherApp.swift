162c3e9a7eab20174e01319ab3e9d832f4b9c1d7
Oleh Kiurchev <okrchv@gmail.com>
2023-01-29 13:22:21 +0200

    chore: initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Oleh Kiurchev on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
