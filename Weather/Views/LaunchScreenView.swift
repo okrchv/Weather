4a71c92e5f04de496fd5802ec5b77f0df8286bba
Oleh Kiurchev <okrchv@gmail.com>
2023-02-05 12:58:15 +0200

    feat: add `LaunchScreenView`

---
+++ Weather/Views/LaunchScreenView.swift
@@ -0,0 +1,27 @@
+//
+//  LaunchScreenView.swift
+//  Weather
+//
+//  Created by Oleh Kiurchev on 04.02.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+            }
+            Spacer()
+        }
+        .background(Color("LightBlue"))
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
